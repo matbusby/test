72da43ecb83be7cf01630e304c42428177b3cb21
Mat Busby <matbusby@gmail.com>
2020-07-24 13:51:51 -0600

    initial commit

---
+++ precache-manifest.edcea791928126bca4f8c52e2249804b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f92a0e93f56906aa64eaeb1f088d1c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4780d397a64c000aba3f",
+    "url": "/static/css/main.c7131295.chunk.css"
+  },
+  {
+    "revision": "7b9bb8f291c938ab15b9",
+    "url": "/static/js/2.832e3c88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.832e3c88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4780d397a64c000aba3f",
+    "url": "/static/js/main.0b886827.chunk.js"
+  },
+  {
+    "revision": "714fd31d90d59547c7bf",
+    "url": "/static/js/runtime-main.0170e32f.js"
+  }
+]);
